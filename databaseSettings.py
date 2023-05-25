ee675704f3d98dc9fd59ee7a32913526c1d378fc
fat83dotcom <fernando.quimic@gmail.com>
2023-05-25 12:41:30 -0300

    Estrutura inicial para banco de dados.

---
+++ databaseSettings.py
@@ -0,0 +1,9 @@
+import confidentials
+
+CONFIG = {
+    'banco_dados': confidentials.bancoDados,
+    'usuario': confidentials.usuario,
+    'host': confidentials.host,
+    'porta': 5432,
+    'senha': confidentials.senha,
+}
--- main.py
+++ main.py
@@ -3,6 +3,93 @@ from itertools import groupby
 from pathlib import Path
 import os
 from sys import getsizeof
+import psycopg2
+from abc import ABC
+from databaseSettings import CONFIG
+
+
+class BancoDeDados(ABC):
+    def __init__(
+            self, host='', port='', dbname='', user='', password=''
+            ) -> None:
+        self.con = psycopg2.connect(
+            host=host, port=port, dbname=dbname, user=user, password=password)
+        self.cursor = self.con.cursor()
+
+    def fecharConexao(self):
+        self.con.close()
+
+    def executar(self, sql):
+        self.cursor.execute(sql)
+
+    def enviar(self):
+        self.con.commit()
+
+    def abortar(self):
+        self.con.rollback()
+
+    def buscarDados(self):
+        return self.cursor.fetchall()
+
+    def buscarUmDado(self):
+        return self.cursor.fetchone()
+
+    def buscarIntervalo(self, intervalo):
+        return self.cursor.fetchmany(intervalo)
+
+    def geradorSQLInsert(self, *args, nome_colunas=None,  nome_tabela=None):
+        valores = args[0]
+        sql = "INSERT INTO %s %s VALUES %s" % (
+            nome_tabela, nome_colunas, valores
+        )
+        return sql
+
+    def geradorSQLUpdate(
+            self, *args, nome_colunas=None, nome_tabela=None, condicao=None
+            ):
+        valores = args[0]
+        sql = "UPDATE %s SET %s='%s' WHERE %s" % (
+            nome_tabela, nome_colunas, valores, condicao
+        )
+        return sql
+
+
+class OperacoesTabelasBD(BancoDeDados):
+
+    def __init__(self, tabela) -> None:
+        self.tablela = tabela
+        self.Bd = BancoDeDados(
+            dbname=CONFIG['banco_dados'],
+            user=CONFIG['usuario'],
+            port=CONFIG['porta'],
+            password=CONFIG['senha'],
+            host=CONFIG['host']
+        )
+
+    def atualizarColuna(self, coluna, condicao, atualizacao):
+        sql = self.geradorSQLUpdate(
+            atualizacao, nome_tabela=self.tablela,
+            nome_colunas=coluna, condicao=condicao)
+        try:
+            self.Bd.executar(sql)
+            self.Bd.enviar()
+        except Exception as e:
+            self.Bd.abortar()
+            raise e
+
+    def inserirColunas(self, *args, coluna):
+        try:
+            sql = self.geradorSQLInsert(
+                *args, nome_colunas=coluna, nome_tabela=self.tablela
+            )
+            self.Bd.executar(sql)
+            self.Bd.enviar()
+        except Exception as e:
+            self.Bd.abortar()
+            raise e
+
+    def fecharConexao(self):
+        return self.Bd.fecharConexao()
 
 
 class FileRetriever:
