cd4acdaaa4fc869003ae2324f472015f16dfb513
fat83dotcom <fernando.quimic@gmail.com>
2023-05-27 19:36:39 -0300

    Algoritmo para padronizar os meses das entradas

---
+++ dateModifier.py
@@ -0,0 +1,30 @@
+import os
+import csv
+from main import FileRetriever
+
+try:
+    pathFolderFiles = '/home/fernando/csv_estacao'
+    r = FileRetriever(pathFolderFiles)
+    path = r.getFoundFiles()
+
+    for fileTarget in path:
+        nameFileTarget = fileTarget
+        with open(fileTarget, 'r', encoding='utf-8') as readFile:
+            modifiedFile = os.path.split(fileTarget)[1]
+            print(modifiedFile)
+            modifiedFile = f'{modifiedFile[:9]}Mod{modifiedFile[9:]}'
+            folderModFileSave = os.path.join(
+                './arquivosModificados', modifiedFile
+            )
+            print(nameFileTarget)
+            print(folderModFileSave)
+            reader = csv.reader((line.replace('\0', '') for line in readFile))
+            with open(folderModFileSave, 'a', encoding='utf-8') as writeFile:
+                write = csv.writer(writeFile)
+                for line in reader:
+                    lineSplit = line[0].split(' ')
+                    lineSplit[1] = lineSplit[1].lower()
+                    line[0] = ' '.join(lineSplit)
+                    write.writerow(line)
+except Exception as e:
+    print(e.__class__.__name__, e)
