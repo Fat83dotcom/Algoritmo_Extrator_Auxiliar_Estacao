d3d1e14e3fe105a5429d1b861ef1983dc3bfc0dc
fat83dotcom <fernando.quimic@gmail.com>
2023-05-27 14:53:31 -0300

    Introdução de um try/except e del para as instancias

--- main.py
+++ main.py
@@ -267,61 +267,67 @@ class DataProcessor:
 
 
 if __name__ == '__main__':
-    bDDataDaily = OperationDataBase('dado_diario')
-    r = FileRetriever('/home/fernando/csv_estacao')
-    dE = DataExtractor()
-    dP = DataProcessor()
-    files = r.getFoundFiles()
-    for currentFile in files:
-        dE.dataExtract(currentFile)
-        extractData = dE.getExtractData()
-        dP.processedData(extractData)
-        dataProcessed = dP.getDataProcessed()
+    try:
+        bDDataDaily = OperationDataBase('dado_diario')
+        r = FileRetriever('/home/fernando/csv_estacao')
+        files = r.getFoundFiles()
+        print(files)
+        for currentFile in files:
+            dE = DataExtractor()
+            dP = DataProcessor()
+            print(currentFile)
+            dE.dataExtract(currentFile)
+            extractData = dE.getExtractData()
+            dP.processedData(extractData)
+            dataProcessed = dP.getDataProcessed()
 
-        for dataDays in dataProcessed:
-            bDDataDaily.insertCollumn(
-                (dataDays['date'],
-                    dataDays['umidity']['minimum'],
-                    dataDays['umidity']['maximum'],
-                    dataDays['umidity']['mean'],
-                    dataDays['umidity']['median'],
-                    dataDays['umidity']['mode'],
-                    dataDays['press']['minimum'],
-                    dataDays['press']['maximum'],
-                    dataDays['press']['mean'],
-                    dataDays['press']['median'],
-                    dataDays['press']['mode'],
-                    dataDays['tempIndoor']['minimum'],
-                    dataDays['tempIndoor']['maximum'],
-                    dataDays['tempIndoor']['mean'],
-                    dataDays['tempIndoor']['median'],
-                    dataDays['tempIndoor']['mode'],
-                    dataDays['tempOutdoor']['minimum'],
-                    dataDays['tempOutdoor']['maximum'],
-                    dataDays['tempOutdoor']['mean'],
-                    dataDays['tempOutdoor']['median'],
-                    dataDays['tempOutdoor']['mode']), collumn='(dia, \
-                    media_umidade, \
-                    minimo_umidade, \
-                    maximo_umidade, \
-                    mediana_umidade, \
-                    moda_umidade, \
-                    media_pressao, \
-                    minimo_pressao, \
-                    maximo_pressao, \
-                    mediana_pressao, \
-                    moda_pressao, \
-                    media_temp_int, \
-                    minimo_temp_int, \
-                    maximo_temp_int, \
-                    mediana_temp_int, \
-                    moda_temp_int, \
-                    media_temp_ext, \
-                    minimo_temp_ext, \
-                    maximo_temp_ext, \
-                    mediana_temp_ext, \
-                    moda_temp_ext\
-                )')
-        print(f'{currentFile} foi salvo no banco de dados.')
+            for dataDays in dataProcessed:
+                bDDataDaily.insertCollumn(
+                    (dataDays['date'],
+                        dataDays['umidity']['minimum'],
+                        dataDays['umidity']['maximum'],
+                        dataDays['umidity']['mean'],
+                        dataDays['umidity']['median'],
+                        dataDays['umidity']['mode'],
+                        dataDays['press']['minimum'],
+                        dataDays['press']['maximum'],
+                        dataDays['press']['mean'],
+                        dataDays['press']['median'],
+                        dataDays['press']['mode'],
+                        dataDays['tempIndoor']['minimum'],
+                        dataDays['tempIndoor']['maximum'],
+                        dataDays['tempIndoor']['mean'],
+                        dataDays['tempIndoor']['median'],
+                        dataDays['tempIndoor']['mode'],
+                        dataDays['tempOutdoor']['minimum'],
+                        dataDays['tempOutdoor']['maximum'],
+                        dataDays['tempOutdoor']['mean'],
+                        dataDays['tempOutdoor']['median'],
+                        dataDays['tempOutdoor']['mode']), collumn='(dia, \
+                        media_umidade, \
+                        minimo_umidade, \
+                        maximo_umidade, \
+                        mediana_umidade, \
+                        moda_umidade, \
+                        media_pressao, \
+                        minimo_pressao, \
+                        maximo_pressao, \
+                        mediana_pressao, \
+                        moda_pressao, \
+                        media_temp_int, \
+                        minimo_temp_int, \
+                        maximo_temp_int, \
+                        mediana_temp_int, \
+                        moda_temp_int, \
+                        media_temp_ext, \
+                        minimo_temp_ext, \
+                        maximo_temp_ext, \
+                        mediana_temp_ext, \
+                        moda_temp_ext\
+                    )')
+            del dE, dP
+            print(f'{currentFile} foi salvo no banco de dados.')
+    except Exception as e:
+        print(e)
 
     bDDataDaily.closeConnection()
