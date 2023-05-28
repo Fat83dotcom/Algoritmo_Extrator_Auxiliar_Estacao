945a289090f59c2bd8ec182f1b453d6a24aebf88
fat83dotcom <fernando.quimic@gmail.com>
2023-05-28 01:31:39 -0300

    try/excetpt para continuação do trabalho após alguma falha de insert

--- main.py
+++ main.py
@@ -327,52 +327,56 @@ if __name__ == '__main__':
             dataProcessed = dP.getDataProcessed()
 
             for dataDays in dataProcessed:
-                bDDataDaily.insertCollumn(
-                    (dataDays['date'],
-                        dataDays['umidity']['minimum'],
-                        dataDays['umidity']['maximum'],
-                        dataDays['umidity']['mean'],
-                        dataDays['umidity']['median'],
-                        dataDays['umidity']['mode'],
-                        dataDays['press']['minimum'],
-                        dataDays['press']['maximum'],
-                        dataDays['press']['mean'],
-                        dataDays['press']['median'],
-                        dataDays['press']['mode'],
-                        dataDays['tempIndoor']['minimum'],
-                        dataDays['tempIndoor']['maximum'],
-                        dataDays['tempIndoor']['mean'],
-                        dataDays['tempIndoor']['median'],
-                        dataDays['tempIndoor']['mode'],
-                        dataDays['tempOutdoor']['minimum'],
-                        dataDays['tempOutdoor']['maximum'],
-                        dataDays['tempOutdoor']['mean'],
-                        dataDays['tempOutdoor']['median'],
-                        dataDays['tempOutdoor']['mode']), collumn='(dia, \
-                        media_umidade, \
-                        minimo_umidade, \
-                        maximo_umidade, \
-                        mediana_umidade, \
-                        moda_umidade, \
-                        media_pressao, \
-                        minimo_pressao, \
-                        maximo_pressao, \
-                        mediana_pressao, \
-                        moda_pressao, \
-                        media_temp_int, \
-                        minimo_temp_int, \
-                        maximo_temp_int, \
-                        mediana_temp_int, \
-                        moda_temp_int, \
-                        media_temp_ext, \
-                        minimo_temp_ext, \
-                        maximo_temp_ext, \
-                        mediana_temp_ext, \
-                        moda_temp_ext\
-                    )')
+                try:
+                    bDDataDaily.toExecute('SET datestyle to ymd')
+                    bDDataDaily.insertCollumn(
+                        (dataDays['date'],
+                            dataDays['umidity']['mean'],
+                            dataDays['umidity']['minimum'],
+                            dataDays['umidity']['maximum'],
+                            dataDays['umidity']['median'],
+                            dataDays['umidity']['mode'],
+                            dataDays['press']['mean'],
+                            dataDays['press']['minimum'],
+                            dataDays['press']['maximum'],
+                            dataDays['press']['median'],
+                            dataDays['press']['mode'],
+                            dataDays['tempIndoor']['mean'],
+                            dataDays['tempIndoor']['minimum'],
+                            dataDays['tempIndoor']['maximum'],
+                            dataDays['tempIndoor']['median'],
+                            dataDays['tempIndoor']['mode'],
+                            dataDays['tempOutdoor']['mean'],
+                            dataDays['tempOutdoor']['minimum'],
+                            dataDays['tempOutdoor']['maximum'],
+                            dataDays['tempOutdoor']['median'],
+                            dataDays['tempOutdoor']['mode']), collumn='(dia, \
+                            media_umidade, \
+                            minimo_umidade, \
+                            maximo_umidade, \
+                            mediana_umidade, \
+                            moda_umidade, \
+                            media_pressao, \
+                            minimo_pressao, \
+                            maximo_pressao, \
+                            mediana_pressao, \
+                            moda_pressao, \
+                            media_temp_int, \
+                            minimo_temp_int, \
+                            maximo_temp_int, \
+                            mediana_temp_int, \
+                            moda_temp_int, \
+                            media_temp_ext, \
+                            minimo_temp_ext, \
+                            maximo_temp_ext, \
+                            mediana_temp_ext, \
+                            moda_temp_ext\
+                        )')
+                except Exception as e:
+                    print(e.__class__.__name__, e)
+                    continue
             del dE, dP
             print(f'{currentFile} foi salvo no banco de dados.')
+        bDDataDaily.closeConnection()
     except Exception as e:
         print(e.__class__.__name__, e)
-
-    bDDataDaily.closeConnection()
