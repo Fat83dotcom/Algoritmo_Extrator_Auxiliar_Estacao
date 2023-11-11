74f481f2451b60e2b6e8cef09b17a02ca3e3249e
fat83dotcom <fernando.quimic@gmail.com>
2023-11-11 20:56:50 -0300

    Retirando o try/except para teste.

--- main.py
+++ main.py
@@ -2,40 +2,22 @@ from classTools import OperationDataBase, FileRetriever, DataExtractor
 from classTools import DataProcessor, DataModel
 
 if __name__ == '__main__':
-    try:
-        dBtable = 'dado_diario'
-        # bDDataDaily = OperationDataBase(dBtable)
-        # bDDataDaily.setBd(1)
-        # bDDataDaily2 = OperationDataBase(dBtable)
-        # bDDataDaily2.setBd(2)
-        bDDataDaily3 = OperationDataBase(dBtable)
-        bDDataDaily3.setBd(3)
-        # dM = DataModel(bDDataDaily)
-        # dM2 = DataModel(bDDataDaily2)
-        dM3 = DataModel(bDDataDaily3)
-        r = FileRetriever('./arquivosModificados')
-        r.findFiles()
-        files = r.getFoundFiles()
-        for currentFile in files:
-            try:
-                dE = DataExtractor()
-                dP = DataProcessor()
-                print(currentFile)
-                dE.dataExtract(currentFile)
-                extractData = dE.getExtractData()
-                dP.processedData(extractData)
-                dataProcessed = dP.getDataProcessed()
-                # dM.executeDB(dataProcessed)
-                # dM2.executeDB(dataProcessed)
-                dM3.executeDB(dataProcessed)
-                del dE, dP
-                print(f'{currentFile} foi salvo no banco de dados.')
-            except Exception as e:
-                print(e.__class__.__name__, e)
-                continue
-
-        # bDDataDaily.closeConnection()
-        # bDDataDaily2.closeConnection()
-        bDDataDaily3.closeConnection()
-    except Exception as e:
-        print(e.__class__.__name__, e)
+    dBtable = 'dado_diario'
+    bDDataDaily = OperationDataBase(dBtable)
+    bDDataDaily.setBd(1)
+    dM3 = DataModel(bDDataDaily)
+    r = FileRetriever('./arquivosModificados')
+    r.findFiles()
+    files = r.getFoundFiles()
+    for currentFile in files:
+        dE = DataExtractor()
+        dP = DataProcessor()
+        print(currentFile)
+        dE.dataExtract(currentFile)
+        extractData = dE.getExtractData()
+        dP.processedData(extractData)
+        dataProcessed = dP.getDataProcessed()
+        dM3.executeDB(dataProcessed)
+        del dE, dP
+        print(f'{currentFile} foi salvo no banco de dados.')
+        bDDataDaily.closeConnection()
